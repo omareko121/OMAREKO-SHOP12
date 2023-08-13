3d7d52f633a7c7de59b282d54dff355b39eb0f0d
divomareko <frtyuiuy665@gmail.com>
2023-08-13 01:42:30 -0700

    first commit

---
+++ backendpage/src/api/prodact/routes/prodact.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * prodact router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::prodact.prodact');
---
+++ backendpage/src/api/prodact/services/prodact.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * prodact service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::prodact.prodact');
